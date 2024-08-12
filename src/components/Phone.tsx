7fd4cde34122f565c1dd4a82be2b2f8c012551c2
Dwaipayan <biswastatay73@gmail.com>
2024-08-13 01:04:41 +0530

    Updated

---
+++ src/components/Phone.tsx
@@ -0,0 +1,13 @@
+import { cn } from "@/lib/utils"
+import { HTMLAttributes } from "react"
+
+interface PhoneProps extends HTMLAttributes<HTMLDivElement>{
+    imgSrc: string
+    dark?: boolean
+}
+
+const Phone = ({imgSrc, className, dark=false, ...props}: PhoneProps)=>{
+    return (<div className={cn("relative pointer-events-none z-50 overflow-hidden")}></div>)
+}
+
+export default Phone
